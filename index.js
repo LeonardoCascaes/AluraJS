007e9fb7ddabbee32d16976b7637216d3d737cd4
Leonardo Cascaes <leonardo.cascaes@hotmail.com>
2022-02-06 18:16:45 -0300

    Adicionado construtor em cliente

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 import {Cliente} from "./Cliente.js";
 import { ContaCorrente } from "./contaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "João";
-cliente1.cpf = "111.222.333-00";
-
-const cliente2 = new Cliente();
-cliente2.nome = "Maria";
-cliente2.cpf = "222.333.444-00";
-
+const cliente1 = new Cliente("João", "111.222.333-00");
 const contaCorrenteJoao = new ContaCorrente();
 contaCorrenteJoao.agencia = 1001;
 contaCorrenteJoao.cliente = cliente1;
 contaCorrenteJoao.depositar(100);
 contaCorrenteJoao.sacar(10);
 
+const cliente2 = new Cliente("Maria", "222.333.444-00");
 const contaCorrenteMaria = new ContaCorrente();
 contaCorrenteMaria.agencia = 1001;
 contaCorrenteMaria.cliente = cliente2;
