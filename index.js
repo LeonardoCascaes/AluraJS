677f32644a33e49a3e382868b04832ed6b9f7ed7
Leonardo Cascaes <leonardo.cascaes@hotmail.com>
2022-02-12 21:26:16 -0300

    Classes e metodos abstratos

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import {Cliente} from "./Cliente.js";
 import { ContaCorrente } from "./contaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaSalario } from "./ContaSalario.js";
 
 const cliente1 = new Cliente("João", "111.222.333-00");
 const contaCorrenteJoao = new ContaCorrente(1001, cliente1);
@@ -13,3 +14,8 @@ console.log(contaCorrenteJoao);
 
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 console.log(contaPoupanca);
+
+const contaSalario = new ContaSalario(cliente2);
+contaSalario.depositar(100);
+contaSalario.sacar(10);
+console.log(contaSalario);
